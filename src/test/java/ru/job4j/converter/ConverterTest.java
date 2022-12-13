dd781d09f75aa7ee686f29d4be60d81c999ef683
ShpiN13 <shpin@bk.ru>
2022-12-13 13:12:01 +0300

    Модульные тесты. [#159435 #370597]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -16,11 +16,10 @@ public class ConverterTest {
 
     @Test
     public void whenConvert200RblThen3dot3333Dlr() {
-        float in = 180;
+        float in = 200;
         float expected = 3;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
     }
-
 }
